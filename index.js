4d7822580527e24bbdaf5fd81fac8575223d2854
Shawn L <coffee.or.tea88@gmail.com>
2017-06-12 01:08:19 +0800

    Edit typo

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/webhook/', function(req, res) {
 })
 
 
-function sendtext(sender, text) {
+function sendText(sender, text) {
 	let messageData = {text: text}
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
